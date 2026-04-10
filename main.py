fee3ef90425f6bad07bdf31011a057be583d2656
Zachary Bomke <zacharybomke@gmail.com>
2026-04-10 20:18:27 +0000

    Set up test api and docker container

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+# Main API source
+from fastapi import FastAPI
+import scorer
+
+app = FastAPI()
+# Origin allow list for API calls
+origins = [
+    "http://localhost:4321",
+    "http://127.0.0.1:4321",
+    "http://localhost:80",
+    "http://127.0.0.1:80",
+    "https://secondlook.venykrid.com"
+]
+
+scorer = scorer.ComplianceScoringEngine 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
